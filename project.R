8004d4c681540351833a30302406f57a47635988
Alexander Parunov <alexander.parunov@gmail.com>
2018-04-30 22:36:50 +0200

    Preprocessed dataset and saved it locally as .Rdata file - Mainly set categorical variables as factors - Gave those factors proper names corresponding to data description on official data website

--- project.R
+++ project.R
@@ -1,21 +1,17 @@
 # database link: https://archive.ics.uci.edu/ml/datasets/default+of+credit+card+clients
 
 #####LIBRARIES######
-api_installed <- require(rstudioapi)
-if(!api_installed) install.packages("rstudioapi")
-ggplot_installed <- require(ggplot2)
-if(!ggplot_installed) install.packages("ggplot2")
-ggplot_installed <- require(plotly)
-if(!ggplot_installed) install.packages("plotly")
-mice_installed <- require(mice)
-if(!mice_installed) install.packages("mice")
+if(!require(rstudioapi)) install.packages("rstudioapi")
+if(!require(ggplot2)) install.packages("ggplot2")
+if(!require(plotly)) install.packages("plotly")
+if(!require(mice)) install.packages("mice")
 
 # Setting working directory as path of current file
 setwd(dirname(getActiveDocumentContext()$path))
 
-Default_Dataset <- read.table("default.csv", header=TRUE, na.strings="?", sep=";") #Importing the data set :D
+Default_Dataset <- read.table("default.csv", header=TRUE, na.strings="?", sep=";") #Importing the data set
 Default_Dataset$ID <- NULL #Prescindible
-summary(Default_Dataset)
+
 #####DESCRIPTION OF THE DATA SET########
 # ID: ID of each client
 # LIMIT_BAL: Amount of given credit in NT dollars (includes individual and family/supplementary credit
@@ -42,66 +38,107 @@ summary(Default_Dataset)
 # PAY_AMT5: Amount of previous payment in May, 2005 (NT dollar)
 # PAY_AMT6: Amount of previous payment in April, 2005 (NT dollar)
 # default.payment.next.month: Default payment (1=yes, 0=no)
+
 #Pre processing missing values.
 sum(is.na(Default_Dataset))
 #There are no missing values in the whole data set. 
 plot(Default_Dataset$PAY_AMT6)
 
-#####SOME PLOTS AND OTHER THINGS IN ORDER TO HAVE A CLEAR IDEA OF THE DATA#######
 summary(Default_Dataset)
-#There are a total of 6636 individuals with default in the next month
-sum(Default_Dataset$default.payment.next.month)
+
+#######PRE PROCESSING ##########
+
+# Set SEX as factor and change levels for more clarity
+Default_Dataset$SEX <- as.factor(Default_Dataset$SEX)
+levels(Default_Dataset$SEX) <- c("male","female")
+
+# MARRIAGE can't be 0, so it's wrong data. Set it as NA and then perform imputation
+Default_Dataset$MARRIAGE[Default_Dataset$MARRIAGE == 0] <- NA
+
+# EDUCATION should be in range [1,4], so any other value is wrong. 
+Default_Dataset$EDUCATION[Default_Dataset$EDUCATION < 1 | Default_Dataset$EDUCATION > 4] <- NA
+
+# Imputing NAs
+imp <- mice(Default_Dataset, m = 1)
+Default_Dataset <- complete(imp)
+
+# Set MARRIAGE as factor and change levels for more clarity
+Default_Dataset$MARRIAGE <- as.factor(Default_Dataset$MARRIAGE)
+levels(Default_Dataset$MARRIAGE) <- c("married","single","other")
+
+# Set EDUCATION as factor and change levels for more clarity
+Default_Dataset$EDUCATION <- as.factor(Default_Dataset$EDUCATION)
+levels(Default_Dataset$EDUCATION) <- c("graduate school", "university", "high school", "other")
+
+# Set default.payment.next.month as factor and change levels for more clarity
+Default_Dataset$default.payment.next.month <- as.factor(Default_Dataset$default.payment.next.month)
+levels(Default_Dataset$default.payment.next.month) <- c("no","yes")
+
+
+# PAY_0,2,3,4,5,6 are categorical variables described above. So we should set them as factors and give proper names
+
+for(i in 6:11) {
+  Default_Dataset[,i] <- as.factor(Default_Dataset[,i])
+  levs <- c("pay duly", "pay duly", "pay duly") # those are -2, -1, 0
+  for(j in 1:8) {
+    st <- paste("delay",j,"months")
+    levs <- c(levs,st)
+  }
+  levels(Default_Dataset[,i]) <- levs
+}
+
+# Saved pre-processed data set for future preprocessing
+save(Default_Dataset, file = "Default_dataset_preprocessed.Rdata")
+
+# We can load data set directly and skip above given steps
+load("Default_dataset_preprocessed.Rdata")
+summary(Default_Dataset)
+
+#####SOME PLOTS AND OTHER THINGS IN ORDER TO HAVE A CLEAR IDEA OF THE DATA#######
+
 #The second column are the individuals that will default. As we can see, most of them are from low education, but, there still some outliers.
 ggplot(Default_Dataset, aes(default.payment.next.month, EDUCATION)) +
   geom_jitter(aes(color = EDUCATION), size = 0.5)
 
-#In this plot we can see that the marital status really do not make a difference and dont tell anything in particular relating the default of the credit card. Here we can see that there are some values for 0 marriage, those values are incorrect and must be corrected.
+#In this plot we can see that the marital status really do not make a difference and dont tell anything in particular relating the default of the credit card.
+# Here we can see that there are some values for 0 marriage, those values are incorrect and must be corrected.
 ggplot(Default_Dataset, aes(default.payment.next.month, MARRIAGE)) +
   geom_jitter(aes(color = MARRIAGE), size = 0.5)
+
 #Same that before but with sex
 ggplot(Default_Dataset, aes(default.payment.next.month, SEX)) +
   geom_jitter(aes(color = SEX), size = 0.5)
 
 
-#######PRE PROCESSING ##########
-sum((Default_Dataset$MARRIAGE==0)) #There are 54 values of marriage that are incorrect. We will set them to null and then interpole the values
-#Setting values to NA
-for (row in 1:nrow(Default_Dataset)) {
-  if(Default_Dataset[row,5] == 0)
-  {
-    Default_Dataset[row,5] = NA
-  }
-}
-sum(is.na(Default_Dataset$MARRIAGE)) #Now we have 54 NA in the Marriage.
-# Imputing NAs
-imp <- mice(Default_Dataset, m = 1)
-Default_Dataset <- complete(imp)
-sum(is.na(Default_Dataset$MARRIAGE)) #Now there is no NA and the valyes have been imputed.
-#Now this plot is shown correctly, we cans ee how the imputation worked and shows the correct information.
-ggplot(Default_Dataset, aes(default.payment.next.month, MARRIAGE)) +
-  geom_jitter(aes(color = MARRIAGE), size = 0.5)
-
 ########PCA#########
-require(FactoMineR)
-PCADefault = PCA(Default_Dataset,quali.sup = 24)
+if(!require(FactoMineR)) install.packages("FactoMineR")
+
+PCADefault <- PCA(Default_Dataset,quali.sup = 24)
 PCADefault
 #Best and worst represented.
 cos1 = PCADefault$ind$cos2 #Return the cos of the individuals
 which.max(cos1[,1]) #The best represented individual is the number 27222.
 which.min(cos1[,1]) #The worst represented individual is the number 7335
+
 #Contributions
-contribution = PCADefault$ind$contrib
-bestinfirstPC = sort(contribution[,1],decreasing = TRUE)[1:3] #Returns the individuals that are more influencial(Contribution) in the first principal component
+contribution <- PCADefault$ind$contrib
+
+bestinfirstPC <- sort(contribution[,1],decreasing = TRUE)[1:3] #Returns the individuals that are more influencial(Contribution) in the first principal component
 bestinfirstPC
-bestinsecondPC = sort(contribution[,2],decreasing = TRUE)[1:3] #Returns the individuals that are more influencial(contribution) in the second principal component
+
+bestinsecondPC <- sort(contribution[,2],decreasing = TRUE)[1:3] #Returns the individuals that are more influencial(contribution) in the second principal component
 bestinsecondPC
+
 #Best represented variables
 cos2 = PCADefault$var$cos2 #Returns the cos of the variables.
 which.max(cos2[,1]) #BILL_AMT4 is the best represented variable in the first factorial plame
 which.min(cos2[,1]) #Marriage is the worst represented variable in the first factorial plame
+
 #Most influencial variables
-contribution2 = PCADefault$var$contrib
-bestinfirstPCvar = sort(contribution2[,1],decreasing = TRUE)[1:3] #Returns the variables that are more influencial(Contribution) in the first principal component
+contribution2 <- PCADefault$var$contrib
+
+bestinfirstPCvar <- sort(contribution2[,1],decreasing = TRUE)[1:3] #Returns the variables that are more influencial(Contribution) in the first principal component
 bestinfirstPCvar
-bestinsecondPCvar = sort(contribution2[,2],decreasing = TRUE)[1:3] #Returns the variables that are more influencial(contribution) in the second principal component
+
+bestinsecondPCvar <- sort(contribution2[,2],decreasing = TRUE)[1:3] #Returns the variables that are more influencial(contribution) in the second principal component
 bestinsecondPCvar
